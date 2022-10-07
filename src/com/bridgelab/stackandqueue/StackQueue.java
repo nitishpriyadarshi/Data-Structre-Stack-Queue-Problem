e3ca86a436320123664d879c401bd9e94b9243fc
nitis <nitish8152@gmail.com>
2022-10-07 13:15:33 +0530

    Added:Welcome Stack Queue Problem

---
+++ src/com/bridgelab/stackandqueue/StackQueue.java
@@ -0,0 +1,9 @@
+package com.bridgelab.stackandqueue;
+
+public class StackQueue {
+    public static void main(String[] args) {
+        //Welcome Message for User
+        System.out.println("Welcome to Stacks and Queues Problems using Linked List.");
+
+    }
+}
